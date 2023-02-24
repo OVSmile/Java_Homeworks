769511a637f5355303b253dd149ee54b0c5114fb
Oleg Smile <valsov-9988@mail.ru>
2023-02-25 01:31:45 +0300

    Added fillArray and print

--- Homework_1/Homework_1.java
+++ Homework_1/Homework_1.java
@@ -1,17 +1,16 @@
 package Homeworks.Homework_1;
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 
 public class Homework_1 {
     private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        int len =getNumberByUser("Введите колличество элементов последовательности треугольных чисел");
+        int len =getNumberByUser("Введите колличество элементов последовательности треугольных чисел: ");
         int[] items = createArray(len);
+        fillArray(items);
         System.out.println(print(items));
     }
-    //Ввод числа и создание массива
+    //Вво5д числа и создание массива
     public static int[] createArray(int size) {
         return new int[size];
     }
@@ -21,5 +20,24 @@ public class Homework_1 {
         return input.nextInt();
     }
 
-    
+    //вывод на печать
+    public static String print(int[] data) {
+        String output = "";
+       
+        for (int i = 0; i < data.length; i++) {
+            output += data[i] + " ";
+        }
+        return output;
+    }
+
+    //Заполнение массива (решение)
+    public static void fillArray(int[] col) {
+        int n = 1;
+        for (int i = 0; i < col.length; i++) {
+            double resalt = (1.0 / 2 * n * (n + 1));
+            col[i] = (int)resalt;
+            n++;
+            System.out.println(resalt);
+        }
+    }
 }
