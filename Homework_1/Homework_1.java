7d975c4257402b94e7b1f0f9c34af22c21f344b0
Oleg Smile <valsov-9988@mail.ru>
2023-02-25 00:21:41 +0300

    Added Create Array

--- Homework_1/Homework_1.java
+++ Homework_1/Homework_1.java
@@ -1,5 +1,25 @@
 package Homeworks.Homework_1;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class Homework_1 {
+    private static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int len =getNumberByUser("Введите колличество элементов последовательности треугольных чисел");
+        int[] items = createArray(len);
+        System.out.println(print(items));
+    }
+    //Ввод числа и создание массива
+    public static int[] createArray(int size) {
+        return new int[size];
+    }
+
+    public static int getNumberByUser(String text) {
+        System.out.print(text);
+        return input.nextInt();
+    }
+
     
 }
